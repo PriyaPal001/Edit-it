5a1ff2bcb3f6a5a287b1491f1d2815308a83d7dc
Priya Pal <vu3t1s2021032@pvppcoe.ac.in>
2025-12-25 19:50:17 +0530

    feat: created user interface with nav and body

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
+	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+  import { goto } from '$app/navigation';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<!--Navbar-->
+
+<div>
+	<nav class="bg-gray-800 p-4 text-white">
+		<div
+			class="container mx-auto flex justify-between items-center"
+		>
+			<div>
+				<a href="/" class="btn btn-ghost text-xl">EditIT</a>
+				<a href="/user" class="btn btn-ghost">My page</a>
+			</div>
+			<div>
+				<button onclick={()=>goto("/login")}>Login</button>
+				<span class="text-white text-lg ml-2">hello1@gmail.com</span>
+				<button onclick={()=>console.log("loging out")}>logout</button>
+			</div>
+		</div>
+	</nav>
+	
+</div>
 
 {@render children()}
---
+++ src/routes/[email]/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import {page} from '$app/stores';
+
+    $: email = $page.params.email;
+</script>
+
+<div class="hero h-screen bg-base-300">
+    <div class="hero-content ">
+        <div class="max-w-2xl text-center">
+            <h1 class="text-5xl font-bold">{email}'s page</h1>
+            <p class="py-3 max-w-md mx-auto">user descp..........</p>
+            <div class="grid grid-cols-3">
+                <div class="card bg-slate-700 m-4 shadow-lg shadow-blue-900">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src={""} alt="" class="w-32 h-32 mx-auto bg-gray-500">
+                            <h2 class="text-white text-2xl font-bold">Pikachu</h2>
+                            <p class="text-info">ELectric</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
